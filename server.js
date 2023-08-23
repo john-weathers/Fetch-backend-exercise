4785770fe02fa75488d2fd86963946194ed72cb9
John Weathers <weathers.johnb@gmail.com>
2023-08-23 10:53:01 -0500

    Add CORS, complete get points middleware

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const { v4: uuid } = require('uuid');
 const { validReceipt, getPoints } = require('./helpers');
 const PORT = process.env.PORT || 8080;
+// object to store ids/receipts
 let receipts = {};
 
+// for the purposes of this app we'll enable all CORS requests
+// if we were building an app that should only be accessed by specific origins we could specifiy cors options here
+app.use(cors());
+// parse incoming requests with JSON payloads
 app.use(express.json());
 
 app.post('/receipts/process', (req, res) => {
-  // destructure all receipt properties
   const { retailer, purchaseDate, purchaseTime, items, total } = req.body;
   
   // check if receipt is valid
@@ -18,7 +23,6 @@ app.post('/receipts/process', (req, res) => {
   const receiptId = uuid();
   receipts[receiptId] = req.body;
   res.status(200).json({ "id": receiptId });
-  
 });
 app.get('/receipts/:id/points', (req, res) => {
   const receiptId = req.params.id;
@@ -27,14 +31,22 @@ app.get('/receipts/:id/points', (req, res) => {
 
   if (!receipt) return res.status(404).json({ "message": "No receipt found for that id" });
 
+  const { retailer, purchaseDate, purchaseTime, items, total } = receipt;
+
+  // pass properties to helper function, return points awarded, send JSON response with points awarded
+  const points = getPoints(retailer, purchaseDate, purchaseTime, items, total);
+  res.status(200).json({ "points": points });
+
 });
 
 app.all('*', (req, res, next) => {
+  // catch all to handle non-matching requests
   res.sendStatus(404);
 });
 app.use((err, req, res, next) => {
+  // very basic error handler
   console.error(err.stack)
   res.status(500).send(err.message);
 });
 
-app.listen(() => console.log(`Server listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
